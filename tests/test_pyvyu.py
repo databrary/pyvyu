3d1108b179dc1c9030f47c76f4c41fb897c504f9
Shohan Hasan <shohan.hasan.x6@gmail.com>
2019-04-19 15:21:15 -0400

    Fix test resources

--- tests/test_pyvyu.py
+++ tests/test_pyvyu.py
@@ -1,12 +1,32 @@
+from distutils import dir_util
+from pytest import fixture
 import pyvyu as pv
+import os
 
+@fixture
+def datadir(tmpdir, request):
+    """""
+    Fixture responsible for searching a folder with the same name of test
+    module and, if available, moving all contents to a temporary directory so
+    tests can use them freely.
+    Code from SO: https://stackoverflow.com/questions/29627341/pytest-where-to-store-expected-data
+    """
+
+    filename = request.module.__file__
+    test_dir, _ = os.path.splitext(filename)
+
+    if os.path.isdir(test_dir):
+        dir_util.copy_tree(test_dir, str(tmpdir))
+
+    return tmpdir
 
 def test_init():
     assert 'load_opf' in dir(pv)
 
 
-def test_load_sample():
-    sheet = pv.load_opf('./DatavyuSampleSpreadsheet.opf')
+def test_load_sample(datadir):
+    testfile = datadir.join('DatavyuSampleSpreadsheet.opf')
+    sheet = pv.load_opf(testfile)
     # print(sheet.get_column_list())
     assert len(sheet.get_column_list()) == 6
 
