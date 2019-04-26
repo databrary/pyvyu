7b6d751ec265457292fc9290f82223313c7dafb5
Shohan Hasan <shohan.hasan.x6@gmail.com>
2019-04-26 16:41:36 -0400

    Update tests from dev merge to work on travis.
    There is still one test that is failing.
    Have to investigate further. But it is an
    assertion fail rather than a configuration error.

--- tests/test_pyvyu.py
+++ tests/test_pyvyu.py
@@ -2,6 +2,7 @@ import pyvyu as pv
 import logging as log
 import os
 import pkg_resources
+import pytest
 
 def get_resource(file):
     """Get test resources. """
@@ -13,28 +14,36 @@ def get_resource(file):
         raise(Exception(f"Can't find resource: {file}"))
 
 
+@pytest.fixture
+def sample_spreadsheet():
+    return get_resource('DatavyuSampleSpreadsheet.opf')
+
+
 def test_init():
     assert 'load_opf' in dir(pv)
 
 
-def test_load_sample():
-    testfile = get_resource('DatavyuSampleSpreadsheet.opf')
-    sheet = pv.load_opf(testfile)
+def test_load_sample(sample_spreadsheet):
+    sheet = pv.load_opf(sample_spreadsheet)
     assert len(sheet.get_column_list()) == 6
 
     momspeech = sheet.get_column('MomSpeech')
-    print([x.ordinal for x in momspeech.sorted_cells()])
     assert len(momspeech.sorted_cells()) == 20
 
 
-def test_spreadsheet_to_df():
-    print("Testing conversion to dataframe...")
-    sheet = pv.load_opf('./DatavyuSampleSpreadsheet.opf')
+def test_spreadsheet_to_df(sample_spreadsheet):
+    sheet = pv.load_opf(sample_spreadsheet)
     df = sheet.to_df()
 
     print(df)
-
     assert(156 == len(df))
+
+    ms = sheet.to_df('MomSpeech')
+    assert(20 == len(ms))
+
+def test_df_to_csv(sample_spreadsheet):
+    sheet = pv.load_opf(sample_spreadsheet)
+    df = sheet.to_df()
     df.to_csv('./DatavyuSampleSpreadsheet.csv')
 
     df = sheet.to_df('MomSpeech')
