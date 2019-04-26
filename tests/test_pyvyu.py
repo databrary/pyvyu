7ba22de0fadfe8d17394322cd59637f72d3dcd0d
Shohan Hasan <shohan.hasan.x6@gmail.com>
2019-04-26 15:51:24 -0400

    Replace fixture with pkg-resource; closes #2

--- tests/test_pyvyu.py
+++ tests/test_pyvyu.py
@@ -1,33 +1,24 @@
-from distutils import dir_util
-from pytest import fixture
 import pyvyu as pv
 import os
+import pkg_resources
 
-@fixture
-def datadir(tmpdir, request):
-    """""
-    Fixture responsible for searching a folder with the same name of test
-    module and, if available, moving all contents to a temporary directory so
-    tests can use them freely.
-    Code from SO: https://stackoverflow.com/questions/29627341/pytest-where-to-store-expected-data
-    """
+def get_resource(file):
+    """Get test resources. """
+    path = os.path.join('resources', file)
 
-    filename = request.module.__file__
-    test_dir, _ = os.path.splitext(filename)
+    if pkg_resources.resource_exists(__name__, path):
+        return pkg_resources.resource_stream(__name__, path)
+    else:
+        raise(Exception(f"Can't find resource: {file}"))
 
-    if os.path.isdir(test_dir):
-        dir_util.copy_tree(test_dir, str(tmpdir))
-
-    return tmpdir
 
 def test_init():
     assert 'load_opf' in dir(pv)
 
 
-def test_load_sample(datadir):
-    testfile = datadir.join('DatavyuSampleSpreadsheet.opf')
+def test_load_sample():
+    testfile = get_resource('DatavyuSampleSpreadsheet.opf')
     sheet = pv.load_opf(testfile)
-    # print(sheet.get_column_list())
     assert len(sheet.get_column_list()) == 6
 
     momspeech = sheet.get_column('MomSpeech')
